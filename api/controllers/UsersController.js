0c13d24b43576535196dd0abf75058293ad1d6ad
web-geek <PremK.Lf@gmail.com>
2019-06-29 12:27:03 +0530

    autentication

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,14 +1,49 @@
 const express = require('express')
 const router = express.Router()
+const _ = require('lodash')
 
-const User = require('../models/User')
+const {User} = require('../models/User')
+const { authenticateUser } = require('../middleware/authenticateUser')
 
 router.post('/register', (req, res) => {
     const body = req.body
     const user = new User(body)
+    // pre-hook --> password encryption
     user.save()
-        .then(user => { res.send(user) })
+        .then(user => {
+            res.send(_.pick(user, ['_id', 'username', 'email', 'createdAt']))
+        })
         .catch(err => { res.send(err) })
+})
+
+router.post('/login', function(req, res) {
+    const body = req.body
+    User.findByCredentials(body.email, body.password)
+        .then(function(user){
+            return user.generateToken()
+        })
+        .then(function(token){
+            res.send({ token })
+        })
+        .catch(function(err){
+            res.send(err)
+        })
+})
+
+router.get('/account', authenticateUser, function(req, res){
+    const { user } = req
+    res.send(_.pick(user, ['_id', 'username', 'email', 'createdAt']))
+})
+
+router.delete('/logout', authenticateUser, function(req, res){
+    const { user, token } = req
+    User.findByIdAndUpdate(user._id, { $pull: {tokens: { token: token }}})
+        .then(function(){
+            res.send({notice: 'successfully logged out'})
+        })
+        .catch(function(err){
+            res.send(err)
+        })
 })
 
 module.exports = {
