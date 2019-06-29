814892eb3b0e17a16be7afb6fbf5e821fc22bdc0
web-geek <PremK.Lf@gmail.com>
2019-06-29 10:42:18 +0530

    address model and controllers

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const port = 3001
 
 const mongoose = require('./config/database')
 const { userRouter } = require('./api/controllers/UsersController')
+const { categoryRouter } = require('./api/controllers/CategoryController')
 
 app.use(express.json())
 
 app.use('/users', userRouter)
+app.use('/categories', categoryRouter)
 
 app.get('/', (req, res) => {
     res.send("eCommerce Application...")
